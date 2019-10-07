84ba686bbd8b479f0a16b6f7e3637ab602db1dbb
Seth Poulin <seth.poulin@gmail.com>
2019-10-07 10:14:48 -0400

    ensured tags are at bottom of post boxes.

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -11,10 +11,54 @@
 <body <?php body_class(); ?> >
   <header>
   </header>
+  <section class="page-copy">
+    <?php $custom_query = new WP_Query('post_type=page&posts_per_page=1'); // show pages only
+      while($custom_query->have_posts()) : $custom_query->the_post(); ?>
+
+          <div class="copy">
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+          </div>
+
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); // reset the query ?>
+
+  </section>
   <section class="posts">
     <ul>
 
-      <?php if (have_posts()) : ?>
+      <?php $custom_query = new WP_Query('posts_per_page=3'); // limit to 3 posts
+        while($custom_query->have_posts()) : $custom_query->the_post(); ?>
+
+          <li class="blogpost">
+
+            <div class="image-container">
+              <?php the_post_thumbnail() ?>
+            </div>
+
+            <div class="copy">
+              <div class="title-date-post">
+                <h2><?php the_title(); ?></h2>
+                <p>Published on: <?php the_time('F j, Y'); ?></p>
+                <?php the_excerpt(); ?>
+              </div>
+              <div class="tags">
+                <h3><?php the_tags(); ?></h3>
+              </div>
+            </div>
+
+          </li>
+
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); // reset the query ?>
+
+    </ul>
+
+
+
+
+
+      <!-- <?php if (have_posts()) : ?>
         <?php while(have_posts()) : the_post(); ?>
 
           <li class="blogpost">
@@ -32,7 +76,7 @@
           </li>
 
         <?php endwhile ; ?>
-      <?php endif; ?>
+      <?php endif; ?> -->
       
     </ul>
   </section>
